8c95ff1dd6183ee9e16cdf678193292258283b35
Jacob Rosenzweig <rosenjcb@gmail.com>
2020-09-05 17:07:40 -0700

    Removed junk

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -18,30 +18,6 @@ export const BlogPostTemplate = ({
 
   return (
     <section className="section">
-      {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map((tag) => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-          </div>
-        </div>
-      </div>
     </section>
   )
 }
